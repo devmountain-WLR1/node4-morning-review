856e6869305d5a460dee0e7d5bdc20aa0df0ccde
Adam Kent <adamkentdev@gmail.com>
2021-05-13 07:02:39 -0700

    register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,21 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    register: (req, res) => {
-        res.sendStatus(200);
+    register: async (req, res) => {
+        const { email, password } = req.body;
+        const db = req.app.get('db');
+
+        let [user] = await db.check_user(email);
+        if (user){
+            return res.status(400).send('Email already exists')
+        }
+
+        let salt = bcrypt.genSaltSyn(10);
+        let hash = bcrypt.hashSync(password, salt);
+        let [newUser] = await db.register_user(email, hash);
+
+        req.session.user = newUser;
+        delete req.session.user.password;
+        res.status(201).send(req.session.user);
     }
 }
